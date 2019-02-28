925a3ec82e6e9867cb0d18dfa6ab42c12f8a32aa
waldirmarques <waldirmarquesdesouza@gmail.com>
2019-02-28 15:40:17 -0300

    Commit

--- C/Uri_1037.c
+++ C/Uri_1037.c
@@ -5,19 +5,17 @@ int main()
     
     scanf("%lf",&entrada);
    
-    if(entrada < 0 || entrada > 100){
+    if(entrada >= 0 && entrada <= 25){
+        printf("Intervalo [0,25]\n");
+    }else if(entrada > 25 && entrada <= 50){
+        printf("Intervalo (25,50]\n");
+    }else if(entrada > 50 && entrada <= 75){
+        printf("Intervalo (50,75]\n");
+    }else if(entrada > 75 && entrada <= 100){
+        printf("Intervalo (75,100]\n");
+    }else {
         printf("Fora de intervalo\n");
     }
    
-    if(entrada >= 0 && entrada <= 25){
-        printf("Intervalo (0,25]\n");
-    }else if(entrada > 25 && entrada <= 50){
-        printf("Intervalo (25,50]\n");
-    }else if(entrada > 50&&entrada <= 75){
-        printf("Intervalo (50,75]\n");
-    }else if(entrada > 75&&entrada <= 100){
-        printf("Intervalo (75,100]\n");
-    }
-
     return 0;
 } 
